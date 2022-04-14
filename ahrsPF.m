024e3ed2f477544411e48db892810df0702b6428
njo0004 <njo0004@auburn.edu>
2022-04-13 23:54:26 -0500

    added z dimension to time update
    enables tracking of individual particles over time

--- ahrsPF.m
+++ ahrsPF.m
@@ -4,6 +4,10 @@ clear
 clc
 close all
 
+% Nicholas Ott questions from paper:
+    % Eq (22) -> what is DCM_*q_hat? (second DCM)
+    % Eq (24) -> what is C?
+    
 %% Data Import
 
 % NOTE: If unable to find file or directory, run dataParser.m or check your
@@ -24,9 +28,11 @@ numSamps = length(gyro); % # of Samples
 %% Particle Filter Parameters
 
 N = 500; % Number of Particles
-q = [ones(1,N); zeros(3,N)]; % Initial Quaternion
+q(:,:,:) = [ones(1,N); zeros(3,N)]; % Initial Quaternion
 [start,stop] = staticGyro(gyro, 0.2); % Static Indices
 sigmaGyro = std(gyro(start:stop,:)); % 
+
+depth = 1; % I want to track how particles evolve over time so add a third dimension to matrix of particles
 
 %% Particle Filter
 
@@ -35,15 +41,17 @@ for i = 1:numSamps
     for j = 1:N
     
         % Time Update
-        gyroP = gyro(i,:) + sigmaGyro*rand; % Particle Gyro
+        gyroP = gyro(i,:) + sigmaGyro*rand; % Particle Gyro (applies the same input plus random noise to all particles)
 
         A = [1 -0.5*gyroP(1)*dt -0.5*gyroP(2)*dt -0.5*gyroP(3)*dt;... 
             0.5*gyroP(1)*dt 1 0.5*gyroP(3)*dt -0.5*gyroP(2)*dt;...
-            0.5*gyroP(2)*dt -0.5*gyroP(3)*dt 1 0.5*gyroP(1)*dt;...
+            0.5*gyroP(2)*dt -2.5*gyroP(3)*dt 1 0.5*gyroP(1)*dt;...
             0.5*gyroP(3)*dt 0.5*gyroP(2)*dt -0.5*gyroP(1)*dt 1];
         
-        q(:,j) = A*q(:,j); % Propagation
+        q(:,j,depth+1) = A*q(:,j,depth); % Propagation
 
     end
+    
+    depth = depth + 1;
 
 end
