68f553438a76f05f471c41d6c21de5753c67e6d2
njo0004 <njo0004@auburn.edu>
2022-04-17 10:39:27 -0500

    changed to quaternion DCM calc

--- dcm_calc.m
+++ dcm_calc.m
@@ -1,16 +1,12 @@
-function out = dcm_calc(roll,pitch,yaw)
+function out = dcm_calc(q)
 
     % purpose of function is to generate the direction cosine matrix (DCM)
     % for the purposes of the measurement update in the particle filter in
     % a magneticlly clean environment
 
-    % CONFIRM THIS:
-psi = roll;
-phi = pitch;
-theta = yaw;
-        
-    out = [cos(theta)*cos(psi) sin(phi)*sin(theta)*cos(psi)-cos(phi)*sin(psi) cos(phi)*sin(theta)*cos(psi)+sin(phi)*sin(psi);...
-           cos(theta)*sin(psi) sin(phi)*sin(theta)*sin(psi)+cos(phi)*cos(psi) cos(phi)*sin(theta)*sin(psi)-sin(phi)*cos(psi);...
-           -sin(theta)         sin(phi)*cos(theta)                            cos(phi)*cos(theta)];
-
+    % Source: Howard's Dissertation:
+    
+    out = [q(1)^2+q(2)^2-q(3)^2-q(4)^2 2*(q(2)*q(3)-q(1)*q(4))     2*(q(1)*q(3)+q(2)*q(4));...
+           2*(q(1)*q(4)+q(2)*q(3))     q(1)^2-q(2)^2+q(3)^2-q(4)^2 2*(q(3)*q(4)-2*q(1)*q(2));...
+           2*(q(2)*q(4)-q(1)*q(3))     2*(q(1)*q(2)+q(3)*q(4))     q(1)^2-q(2)^2-q(3)^2+q(4)^2];
 end
