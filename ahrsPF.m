5db3562405d5519ebc7c3b6bba5e45421c740c44
njo0004 <njo0004@auburn.edu>
2022-04-20 14:25:10 -0500

    resampling is WIP

--- ahrsPF.m
+++ ahrsPF.m
@@ -9,7 +9,20 @@ close all
     % Eq (24) -> what is C?
     % Eq (15) -> we need to define DCM (this is probably simple I am just
         % not there yet
-    
+        
+% Resampling Thoughts:
+    % Effective Sample Size method
+        % when effective sample size gets below threshold, then resampling
+        % is needed
+        % cv = (1/N)*sum(N*w(i)-1)^2 from i = 1 -> N
+        % ESS = M/(1+cv^2)
+        
+        % When ESS < ESS_threshold, perform resampling
+        % ESS_threshold = 0.5*N (50% of particles are ineffective)
+        
+% Resampling algorithm is it's own function
+
+
     
 %% Data Import
 
@@ -35,6 +48,9 @@ numSamps = length(gyro); % # of Samples
 %% Particle Filter Parameters
 
 N = 1000; % Number of Particles
+
+ESS_thresh = 0.5*N; % when 50% of particles are "ineffective" resample
+
 qP = [ones(1,N); zeros(3,N)]; % Initial Quaternions for Particles
 [start,stop] = staticGyro(gyro, 0.2); % Static Indices
 sigmaGyro = std(gyro(start:stop,:)); % 
@@ -54,7 +70,7 @@ for i = 1:numSamps-1
 
         % Time Update (think this is also wrong)
         samp = gyro(i,:)' + sigmaGyro'.*rand(3,1); % uniform distribution (mean is gyro noise, variance is noise floor)
-        qP(:,j) = q_hat(:,i); 
+        qP(:,j) = qP(:,i); 
         gyroP = gyro(i,:)' + samp; % Particle Gyro (resampling based on gyro measurements and noise parameters)
         
         % thoughts: quaternion particle is the mean from the previous time
@@ -99,14 +115,25 @@ for i = 1:numSamps-1
     
     W = W./sum(W); % normalize weights
     
+    q_hat(:,i+1) = zeros(4,1); % fill state estimate with zeros to allow summation
+    temp = 0;
+    
     for j = 1:N
         
-        q_calc = q_calc + W(j)*qP(:,j); % I dont think this is wrong
-           
+        q_hat(:,i+1) = q_hat(:,i+1) + W(j)*qP(:,j); % I dont think this is wrong
+        
+        % --- Resampling --- %
+        temp = temp + (N*W(j)-1)^2;
+%       cv = (1/N)*sum(N*w(i)-1)^2   
     end
     
-    q_hat(:,i+1) = q_calc; % state estimate is overall weighted sum 
+    cv = (1/N)*temp;
+    ESS = N/(1+cv^2);
     
+    if ESS<ESS_thresh
+       break 
+    end
+        
     q_calc = 0;
         
 end
@@ -114,41 +141,41 @@ end
 disp('ended')
 %% Converting to Euler Angles
 
-[r,c] = size(q_hat);
-
-q_hat = q_hat';
-
-for i = 1:c
-    eul(i,:) = quat2eul(q_hat(i,:));
-end
-
-[r,c] = size(orient);
-
-for i = 1:r
-   truth(i,:) = quat2eul(orient(i,:)); 
-end
-
-yaw = rad2deg(eul(:,1));
-pitch = rad2deg(eul(:,2));
-roll = rad2deg(eul(:,3));
-
-yaw_truth = rad2deg(truth(:,1));
-pitch_truth = rad2deg(truth(:,2));
-roll_truth = rad2deg(truth(:,3));
-
-figure()
-subplot(3,1,1)
-plot(roll,'.')
-hold on
-plot(roll_truth,'.')
-title('Roll')
-subplot(3,1,2)
-plot(pitch,'.')
-hold on
-plot(pitch_truth,'.')
-title('Pitch')
-subplot(3,1,3)
-plot(yaw,'.')
-hold on
-plot(yaw_truth,'.')
-title('Yaw')
+% [r,c] = size(q_hat);
+% 
+% q_hat = q_hat';
+% 
+% for i = 1:c
+%     eul(i,:) = quat2eul(q_hat(i,:));
+% end
+% 
+% [r,c] = size(orient);
+% 
+% for i = 1:r
+%    truth(i,:) = quat2eul(orient(i,:)); 
+% end
+% 
+% yaw = rad2deg(eul(:,1));
+% pitch = rad2deg(eul(:,2));
+% roll = rad2deg(eul(:,3));
+% 
+% yaw_truth = rad2deg(truth(:,1));
+% pitch_truth = rad2deg(truth(:,2));
+% roll_truth = rad2deg(truth(:,3));
+% 
+% figure()
+% subplot(3,1,1)
+% plot(roll,'.')
+% hold on
+% plot(roll_truth,'.')
+% title('Roll')
+% subplot(3,1,2)
+% plot(pitch,'.')
+% hold on
+% plot(pitch_truth,'.')
+% title('Pitch')
+% subplot(3,1,3)
+% plot(yaw,'.')
+% hold on
+% plot(yaw_truth,'.')
+% title('Yaw')
